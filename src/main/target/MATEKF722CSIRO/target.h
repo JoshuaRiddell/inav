156fdec9c5db1f774e36e5fcb2d77f8ec10d6899
Riddell, Josh (Data61, Pullenvale) <Josh.Riddell@data61.csiro.au>
2019-05-15 23:28:26 +1000

    Remove old softi2c code from target

--- src/main/target/MATEKF722CSIRO/target.h
+++ src/main/target/MATEKF722CSIRO/target.h
@@ -72,23 +72,14 @@
 #define I2C1_SCL                PB8
 #define I2C1_SDA                PB9
 
-// #define USE_I2C
-// #define USE_I2C_DEVICE_EMULATED
-// #define SOFT_I2C
-// #define SOFT_I2C_SCL                PB8
-// #define SOFT_I2C_SDA                PB9
-
-#define DEFAULT_I2C_BUS         BUS_I2C1
-// #define DEFAULT_I2C_BUS         BUS_I2C_EMULATED
-
 #define USE_BARO
-#define BARO_I2C_BUS            DEFAULT_I2C_BUS
+#define BARO_I2C_BUS            BUS_I2C1
 #define USE_BARO_BMP280
 #define USE_BARO_MS5611
 #define USE_BARO_MS583730
 
 #define USE_MAG
-#define MAG_I2C_BUS             DEFAULT_I2C_BUS
+#define MAG_I2C_BUS             BUS_I2C1
 #define USE_MAG_HMC5883
 #define USE_MAG_QMC5883
 #define USE_MAG_IST8310
@@ -96,12 +87,12 @@
 #define USE_MAG_MAG3110
 #define USE_MAG_LIS3MDL
 
-#define TEMPERATURE_I2C_BUS     DEFAULT_I2C_BUS
+#define TEMPERATURE_I2C_BUS     BUS_I2C1
 
-#define PITOT_I2C_BUS           DEFAULT_I2C_BUS
+#define PITOT_I2C_BUS           BUS_I2C1
 
 #define USE_RANGEFINDER
-#define RANGEFINDER_I2C_BUS     DEFAULT_I2C_BUS
+#define RANGEFINDER_I2C_BUS     BUS_I2C1
 
 // *************** SPI2 OSD ***********************
 #define USE_SPI_DEVICE_2
